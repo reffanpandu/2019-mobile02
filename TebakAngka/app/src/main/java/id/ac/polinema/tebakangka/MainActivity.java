b9d84a889141f06357a105a24ccaf8d8162a5830
reffanpanduamirulloh <reffan014@gmail.com>
2019-09-11 21:30:46 +0700

    menambah todo tebak angka

--- TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
+++ TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
@@ -10,27 +10,69 @@ import android.widget.Toast;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
 	//  TODO: deklarasikan variabel di sini
 
+	EditText inputNumber;
+	int randomNumber;
+	Button tebakButton;
+	Button resetButton;
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		// TODO: bind layout di sini
+
+		inputNumber = findViewById(R.id.number_input);
+		tebakButton = findViewById(R.id.guess_button);
+		resetButton = findViewById(R.id.reset_button);
+
+		initRandomNumber();
 	}
 
 	// TODO: generate angka random di sini
-	private void initRandomNumber() {
+	private void initRandomNumber()
+	{
+		Random angkaRandom = new Random();
+		randomNumber = 1 + angkaRandom.nextInt((100 - 1) + 1);
 
 	}
 
-	public void handleGuess(View view) {
+	public void handleGuess(View view)
+	{
 		// TODO: Tambahkan logika untuk melakukan pengecekan angka
+		if (!inputNumber.getText().toString().equals("-") && !inputNumber.getText().toString().equals(""))
+		{
+			int input = Integer.parseInt(inputNumber.getText().toString());
+			if (input < randomNumber)
+			{
+				Toast.makeText(this, "Tebakan anda terlalu kecil!", Toast.LENGTH_SHORT).show();
+			}
+			else if (input > randomNumber)
+			{
+				Toast.makeText(this, "Tebakan anda terlalu besar!", Toast.LENGTH_SHORT).show();
+			}
+			else
+			{
+				Toast.makeText(this, "Tebakan anda benar!", Toast.LENGTH_SHORT).show();
+				tebakButton.setEnabled(false);
+			}
+		}
+		else
+		{
+			Toast.makeText(this, "Masukkan angka yang ditebak!", Toast.LENGTH_SHORT).show();
+		}
 	}
 
-	public void handleReset(View view) {
+	public void handleReset(View view)
+	{
 		// TODO: Reset tampilan
+		initRandomNumber();
+		inputNumber.setText("");
+		tebakButton.setEnabled(true);
 	}
 }
